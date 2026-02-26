629f92cb431e0957eda48e5401093a1e10ebee1a
𒋲𝚫𝚪𝚰𝚳𝚫𒋲 <hadeslord087@gmail.com>
2026-02-26 10:56:51 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,23 @@
+// search.js
 
+function initSearchSystem() {
+
+    const searchInput = document.querySelector('.search-input');
+    const searchSuggestions = document.querySelector('.search-suggestions');
+
+    if (!searchInput || !searchSuggestions) return;
+
+    searchInput.addEventListener('focus', () => {
+        searchSuggestions.style.display = 'block';
+    });
+
+    searchInput.addEventListener('blur', () => {
+
+        setTimeout(() => {
+            searchSuggestions.style.display = 'none';
+        }, 200);
+
+    });
+}
+
+document.addEventListener("DOMContentLoaded", initSearchSystem);
